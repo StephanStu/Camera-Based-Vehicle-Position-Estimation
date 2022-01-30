bb9d20f83d8e527c11a99660730fbd3c9b39b371
Stephan Studener <stephan.studener@lilium.com>
2022-01-30 20:27:16 +0100

    Created a test program to verfify OpenCV - a) esnure it works and all necessary servicesdrs are here - b) to run Hough Trannsfromation with the test images

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,45 @@
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/mat.hpp>
+#include <vector>
 #include <iostream>
  
 int main( int argc, char** argv ) {
+  cv::Mat image; // used to hold an RGB-image
+  cv::Mat grayImage; // used for the result of transfroming the RGB-Image to a grayscale-image
+  cv::Mat edgesDetected; // used for the result of the Canny-Edge-Detection
+  std::vector<cv::Vec4i> lines; // a vector to store lines of the image, using the cv::Vec4i-Datatype
+
+
   
-  cv::Mat image;
   image = cv::imread("SimpleRunwayTestImage.png" ,cv::IMREAD_COLOR);
   
   if(! image.data ) {
-      std::cout <<  "Image not found or unable to open" << std::endl ;
-      return -1;
+    std::cout <<  "Image not found or unable to open" << std::endl ;
+    return -1;
+  }else{
+    /*
+    Now convert this image to a gray image
+    */
+    cv::cvtColor(image, grayImage, cv::COLOR_BGR2GRAY);
+    /*
+    Run the canny-edge-detection 
+    */
+    cv::Canny(grayImage, edgesDetected, 50, 200);
+    /*
+    Run the Hough-Transformation
+    */
+    cv::HoughLinesP(edgesDetected, lines, 1, CV_PI/180, 250, 100, 250);
+    // Draw lines on the image
+    for (size_t i=0; i<lines.size(); i++) {
+      cv::Vec4i l = lines[i];
+      cv::line(image, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(255, 255, 255), 3, cv::LINE_AA);
     }
+
+  }
   
-  cv::namedWindow( "Techawarey:OpenCV Test Program", cv::WINDOW_AUTOSIZE );
-  cv::imshow( "Techawarey:OpenCV Test Program", image );
+  cv::namedWindow( "OpenCV Test Program", cv::WINDOW_AUTOSIZE );
+  cv::imshow( "OpenCV Test Program", image );
   
   cv::waitKey(0);
   return 0;
